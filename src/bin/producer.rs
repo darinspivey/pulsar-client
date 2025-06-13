e76260666b8db863de7a94483d4fa8998071d45d
Darin Spivey <darin.spivey@mezmo.com>
2025-06-13 14:44:04 -0400

    Add http support for posting to an endpoint

--- src/bin/producer.rs
+++ src/bin/producer.rs
@@ -1,15 +1,23 @@
 #![allow(unused_imports)]
-use clap::Parser;
+use clap::{ArgGroup, Parser};
+use hyper::{Body, Client, Request, Uri};
+use hyper_tls::HttpsConnector;
 use log::{debug, error, info, warn};
 use pulsar::{
     message::proto, producer, Error as PulsarError, Pulsar, SerializeMessage, TokioExecutor,
 };
 use serde::{Deserialize, Serialize};
+use std::str::FromStr;
 
 // FIXME: Requires local port-forwarding `kubectl port-forward service/pulsar-proxy 6650:6650 -n pipeline`
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
+#[clap(group(
+    ArgGroup::new("destination")
+        .required(true)
+        .args(&["topic", "http_endpoint"]),
+))]
 struct Args {
     /// Number of messages to publish
     #[arg(short, long, default_value_t = 1)]
@@ -20,12 +28,16 @@ struct Args {
     size: usize,
 
     /// Topic to publish messages to
-    #[arg(
-        short,
-        long,
-        default_value = "persistent://your_tenant/your_namespace/your_topic"
-    )]
-    topic: String,
+    #[arg(short, long)]
+    topic: Option<String>,
+
+    /// HTTP endpoint to publish messages to
+    #[arg(long)]
+    http_endpoint: Option<String>,
+
+    /// Authorization token for HTTP requests
+    #[arg(long)]
+    auth_token: Option<String>,
 }
 
 #[derive(Serialize, Deserialize)]
@@ -50,41 +62,96 @@ fn generate_large_data(num: usize, size_kb: usize) -> String {
     base_string.repeat(repetitions)
 }
 
+type HttpClient = Client<HttpsConnector<hyper::client::HttpConnector>>;
+
+fn create_http_client() -> HttpClient {
+    let https = HttpsConnector::new();
+    Client::builder().build(https)
+}
+
+async fn send_to_http(
+    client: &HttpClient,
+    endpoint: &str,
+    data: &TestData,
+    auth_token: Option<&str>,
+) -> Result<(), Box<dyn std::error::Error>> {
+    let uri = Uri::from_str(endpoint)?;
+    let body = serde_json::to_string(data)?;
+    let mut req = Request::post(uri).header("content-type", "application/json");
+
+    if let Some(token) = auth_token {
+        req = req.header("Authorization", format!("Bearer {}", token));
+    }
+
+    let req = req.body(Body::from(body))?;
+
+    let resp = client.request(req).await?;
+    if !resp.status().is_success() {
+        return Err(format!("HTTP request failed with status: {}", resp.status()).into());
+    }
+    Ok(())
+}
+
 #[tokio::main]
-async fn main() -> Result<(), pulsar::Error> {
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     env_logger::init();
     let args = Args::parse();
 
-    let addr = "pulsar://localhost:6650";
-    let pulsar: Pulsar<_> = Pulsar::builder(addr, TokioExecutor).build().await?;
-    let mut producer = pulsar
-        .producer()
-        .with_topic(&args.topic) // Use the topic from command-line arguments
-        .with_name("my producer")
-        .with_options(producer::ProducerOptions {
-            schema: Some(proto::Schema {
-                r#type: proto::schema::Type::String as i32,
-                ..Default::default()
-            }),
-            ..Default::default()
-        })
-        .build()
-        .await?;
+    let use_http = args.http_endpoint.is_some();
+    let mut pulsar_producer = None;
+    let http_client = create_http_client();
 
-    // let large_data = generate_large_data(args.size);
+    if !use_http {
+        if let Some(topic) = &args.topic {
+            let addr = "pulsar://localhost:6650";
+            let pulsar: Pulsar<_> = Pulsar::builder(addr, TokioExecutor).build().await?;
+            pulsar_producer = Some(
+                pulsar
+                    .producer()
+                    .with_topic(topic)
+                    .with_name("my producer")
+                    .with_options(producer::ProducerOptions {
+                        schema: Some(proto::Schema {
+                            r#type: proto::schema::Type::String as i32,
+                            ..Default::default()
+                        }),
+                        ..Default::default()
+                    })
+                    .build()
+                    .await?,
+            );
+        }
+    }
 
     for i in 0..args.count {
-        producer
-            .send(TestData {
-                data: generate_large_data(i, args.size),
-            })
+        let data = TestData {
+            data: generate_large_data(i, args.size),
+        };
+
+        if use_http {
+            send_to_http(
+                &http_client,
+                args.http_endpoint.as_ref().unwrap(),
+                &data,
+                args.auth_token.as_deref(),
+            )
             .await?;
+        } else {
+            if let Some(producer) = &mut pulsar_producer {
+                producer.send(data).await?;
+            }
+        }
 
         info!(
-            "Sent message {} of {} (size: {}KB)",
+            "Sent message {} of {} (size: {}KB) to {}",
             i + 1,
             args.count,
-            args.size
+            args.size,
+            if use_http {
+                "HTTP endpoint"
+            } else {
+                "Pulsar topic"
+            }
         );
     }
 
